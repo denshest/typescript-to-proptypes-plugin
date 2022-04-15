710084fb4e757d16c5e50dcad1ffd13552a7bf99
GitHub Action <action@github.com>
2022-04-15 11:58:20 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/denshest/typescripttoproptypesplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.denshest.typescripttoproptypesplugin.services
+
+import com.github.denshest.typescripttoproptypesplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/denshest/typescripttoproptypesplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/denshest/typescripttoproptypesplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.denshest.typescripttoproptypesplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.denshest.typescripttoproptypesplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
